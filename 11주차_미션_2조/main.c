6d08b8540c47c227416ad67b40f7f2740102f0a4
shasuri <ghimmk@gmail.com>
2021-12-17 13:45:25 +0900

    Recover comments and save by utf8

--- 11주차_미션_2조/main.c
+++ 11주차_미션_2조/main.c
@@ -13,13 +13,13 @@
 #include "touch.h"
 #include "touch.c"
 
-int color[12] = {WHITE,CYAN,BLUE,RED,MAGENTA,LGRAY,GREEN,YELLOW,BROWN,BRRED,GRAY};
+int color[12] = {WHITE, CYAN, BLUE, RED, MAGENTA, LGRAY, GREEN, YELLOW, BROWN, BRRED, GRAY};
 uint16_t value = 100;
 int flag = 1;
 int ledCount = 0;
 
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-TIM_OCInitTypeDef       TIM_OCInitStructure;
+TIM_OCInitTypeDef TIM_OCInitStructure;
 uint16_t prescale;
 
 /* function prototype */
@@ -31,27 +31,28 @@ void NVIC_Configure(void);
 
 void RCC_Configure(void)
 {
-      /* LCD */
-      RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // port C RCC ENABLE
-      /* PWM */
-      RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // TIM2
-      /* LED */
-      RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE); // Port D
+   /* LCD */
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // port C RCC ENABLE
+   /* PWM */
+   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // TIM2
+   /* LED */
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE); // Port D
 
    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
 
 void GPIO_Configure(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure3;
-    GPIO_InitStructure3.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_7;
-    GPIO_InitStructure3.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure3.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure3); // led »∞º∫»≠
+   GPIO_InitTypeDef GPIO_InitStructure3;
+   GPIO_InitStructure3.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_7;
+   GPIO_InitStructure3.GPIO_Speed = GPIO_Speed_50MHz;
+   GPIO_InitStructure3.GPIO_Mode = GPIO_Mode_Out_PP;
+   GPIO_Init(GPIOD, &GPIO_InitStructure3); // led ÌôúÏÑ±Ìôî
 }
 
-void NVIC_Configure(void) {
-    
+void NVIC_Configure(void)
+{
+
    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
    NVIC_InitTypeDef NVIC_InitStructure;
 
@@ -63,86 +64,97 @@ void NVIC_Configure(void) {
    NVIC_Init(&NVIC_InitStructure);
 }
 
-void TIM2_IRQHandler(void) {
-  if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
-  {
-      if (ledCount % 2 == 0) {
-      GPIO_SetBits(GPIOD, GPIO_Pin_4);
-      } else{
-      GPIO_ResetBits(GPIOD, GPIO_Pin_4); 
+void TIM2_IRQHandler(void)
+{
+   if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+   {
+      if (ledCount % 2 == 0)
+      {
+         GPIO_SetBits(GPIOD, GPIO_Pin_4);
+      }
+      else
+      {
+         GPIO_ResetBits(GPIOD, GPIO_Pin_4);
       }
 
-      if(ledCount == 0) {
-      GPIO_ResetBits(GPIOD, GPIO_Pin_7);
-      } else if(ledCount == 5) {
-      GPIO_SetBits(GPIOD, GPIO_Pin_7);
+      if (ledCount == 0)
+      {
+         GPIO_ResetBits(GPIOD, GPIO_Pin_7);
+      }
+      else if (ledCount == 5)
+      {
+         GPIO_SetBits(GPIOD, GPIO_Pin_7);
       }
 
       ledCount++;
       ledCount %= 10;
 
       TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-  }
+   }
 }
 
 void PWM_Configure()
 {
-   prescale = (uint16_t) (SystemCoreClock / 10000);
+   prescale = (uint16_t)(SystemCoreClock / 10000);
    TIM_TimeBaseStructure.TIM_Period = 10000;
    TIM_TimeBaseStructure.TIM_Prescaler = prescale;
    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
-   
+
    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
    TIM_ARRPreloadConfig(TIM2, ENABLE);
    TIM_Cmd(TIM2, ENABLE);
 }
 
-
 int main(void)
 {
 
-    SystemInit();
-    RCC_Configure();
-    GPIO_Configure();
-    PWM_Configure();
-    NVIC_Configure();
+   SystemInit();
+   RCC_Configure();
+   GPIO_Configure();
+   PWM_Configure();
+   NVIC_Configure();
 
-    //-----------------
+   //-----------------
 
-    LCD_Init(); // LCD √ ±‚»≠
-    Touch_Configuration(); // ≈Õƒ° º≥¡§
-    Touch_Adjust(); // »≠∏È ≈Õƒ° √ ¡° ∏¬√ﬂ±‚
-    LCD_Clear(WHITE); // LCD πË∞Ê √ ±‚»≠
+   LCD_Init();            // LCD Ï¥àÍ∏∞Ìôî
+   Touch_Configuration(); // ÌÑ∞Ïπò ÏÑ§Ï†ï
+   Touch_Adjust();        // ÌôîÎ©¥ ÌÑ∞Ïπò Ï¥àÏ†ê ÎßûÏ∂îÍ∏∞
+   LCD_Clear(WHITE);      // LCD Î∞∞Í≤Ω Ï¥àÍ∏∞Ìôî
 
-    //-----------------
+   //-----------------
 
-    uint16_t pos_temp[2]; // x, y¡¬«•∏¶ ¥„¿ª πËø≠
+   uint16_t pos_temp[2]; // x, yÏ¢åÌëúÎ•º Îã¥ÏùÑ Î∞∞Ïó¥
 
-   LCD_ShowString(40, 10, "MON_Team02", MAGENTA, WHITE); // ∆¿∏Ì √‚∑¬
-   LCD_ShowString(90, 50, "OFF", RED, WHITE); // µ∆˙∆Æ∑Œ OFF 
-   LCD_DrawRectangle(40, 80, 80, 120); // ªÁ∞¢«¸ √‚∑¬
-   LCD_ShowString(60,100, "Button", MAGENTA, WHITE); // "Button" ±€¿⁄ √‚∑¬
-    
-    while (1) {
-         Touch_GetXY(&pos_temp[0], &pos_temp[1], 1); // ≈Õƒ° ¡¬«• πﬁæ∆º≠ πËø≠ø° ¿‘∑¬
-         Convert_Pos(pos_temp[0],pos_temp[1],&pos_temp[0],&pos_temp[1]); // πﬁ¿∫ ¡¬«•∏¶ LCD ≈©±‚ø° ∏¬∞‘ ∫Ø»Ø
-         printf("%d, %d\n", pos_temp[0], pos_temp[1]);
+   LCD_ShowString(40, 10, "MON_Team02", MAGENTA, WHITE); // ÌåÄÎ™Ö Ï∂úÎ†•
+   LCD_ShowString(90, 50, "OFF", RED, WHITE);            // ÎîîÌè¥Ìä∏Î°ú OFF
+   LCD_DrawRectangle(40, 80, 80, 120);                   // ÏÇ¨Í∞ÅÌòï Ï∂úÎ†•
+   LCD_ShowString(60, 100, "Button", MAGENTA, WHITE);    // "Button" Í∏ÄÏûê Ï∂úÎ†•
 
-         if ((uint16_t)40 < pos_temp[0] && pos_temp[0] < (uint16_t)80 && \
-             (uint16_t)80 < pos_temp[1] && pos_temp[1] < (uint16_t)120) {
-            if (flag) {
-               LCD_ShowString(90, 50, "    ", RED, WHITE); 
-               LCD_ShowString(40, 50, "ON", RED, WHITE); 
-               TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // interrupt enable
-            } else {
-               LCD_ShowString(90, 50, "OFF", RED, WHITE); 
-               LCD_ShowString(40, 50, "    ", RED, WHITE); 
-               TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE); // interrupt enable
-            }
-            flag++;
-            flag %= 2;
+   while (1)
+   {
+      Touch_GetXY(&pos_temp[0], &pos_temp[1], 1);                        // ÌÑ∞Ïπò Ï¢åÌëú Î∞õÏïÑÏÑú Î∞∞Ïó¥Ïóê ÏûÖÎ†•
+      Convert_Pos(pos_temp[0], pos_temp[1], &pos_temp[0], &pos_temp[1]); // Î∞õÏùÄ Ï¢åÌëúÎ•º LCD ÌÅ¨Í∏∞Ïóê ÎßûÍ≤å Î≥ÄÌôò
+      printf("%d, %d\n", pos_temp[0], pos_temp[1]);
+
+      if ((uint16_t)40 < pos_temp[0] && pos_temp[0] < (uint16_t)80 &&
+          (uint16_t)80 < pos_temp[1] && pos_temp[1] < (uint16_t)120)
+      {
+         if (flag)
+         {
+            LCD_ShowString(90, 50, "    ", RED, WHITE);
+            LCD_ShowString(40, 50, "ON", RED, WHITE);
+            TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // interrupt enable
          }
-    }
-    return 0;
+         else
+         {
+            LCD_ShowString(90, 50, "OFF", RED, WHITE);
+            LCD_ShowString(40, 50, "    ", RED, WHITE);
+            TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE); // interrupt enable
+         }
+         flag++;
+         flag %= 2;
+      }
+   }
+   return 0;
 }
